3ead0d4b567440f1377533d9641f9a9e62e289f2
zakky <sadaoza@gmail.com>
2024-04-14 16:01:11 +0900

    ChatGPTと連携

---
+++ chatGPT.js
@@ -0,0 +1,27 @@
+function chatGPT(prompt) {
+  prompt = "こんにちは";
+  const constraints = SHEET.getRange(1, 1).getValue(); // 制約
+  // totalMessages.unshift({"role": "system", "content": constraints});
+  const requestOptions = {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + OPENAI_APIKEY,
+    },
+    payload: JSON.stringify({
+      model: CHAT_GPT_VER,
+      // "messages": totalMessages,
+      messages: [
+        { role: "system", content: constraints }, // Who you are?
+        { role: "user", content: prompt }, // prompt
+      ],
+    }),
+  };
+  const response = UrlFetchApp.fetch(CHAT_GPT_URL, requestOptions);
+  const responseText = response.getContentText();
+  const json = JSON.parse(responseText);
+  // console.log(json);
+  return_text = json["choices"][0]["message"]["content"].trim();
+  Logger.log(`出力メッセージ：${return_text}`);
+  return return_text;
+}
---
+++ config.js
@@ -0,0 +1,20 @@
+// API設定部分
+// const OPENAI_APIKEY     = "sk-***********************************************";
+// const LINE_ACCESS_TOKEN = "***********************************************";
+const PROPS = PropertiesService.getScriptProperties();
+const OPENAI_APIKEY = PROPS.getProperty('OPENAI_APIKEY');
+const LINE_ACCESS_TOKEN = PROPS.getProperty('LINE_ACCESS_TOKEN');
+
+// 使用API
+const LINE_REPLY_URL = 'https://api.line.me/v2/bot/message/reply';
+const CHAT_GPT_URL   = "https://api.openai.com/v1/chat/completions";
+// const CHAT_GPT_VER   = "gpt-3.5-turbo"; // 2023年11月現在、前のモデル（gpt-3.5-turbo-0613）が使用されているため、1行下の最新モデルを使用
+const CHAT_GPT_VER   = "gpt-3.5-turbo-1106"; // 一度のやり取りで16000トークンまで使用可能
+
+// スプレッドシートの情報
+const SS         = SpreadsheetApp.getActiveSpreadsheet();
+const SHEET      = SS.getSheetByName('制約');
+const SHEET_LOG  = SS.getSheetByName('ログ'); // 追加
+const SHEET_USER = SS.getSheetByName('ユーザー'); // 追加
+
+const MAX_COUNT_LOG = 3; // 過去のLINEでのメッセージを遡る回数
